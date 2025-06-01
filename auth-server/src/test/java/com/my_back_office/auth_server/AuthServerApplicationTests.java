268b85e7de051af59bce5402f0d4fbe8ce625236
정호원 <jjhw0106@naver.com>
2025-06-01 19:43:33 +0900

    jwt인증 구현

---
+++ auth-server/src/test/java/com/my_back_office/auth_server/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_back_office.auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
